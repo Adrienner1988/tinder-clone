fd88fe1ac1347366220fe261ed091e47c9feb2c6
adrienner1988 <adriennerdaniels@gmail.com>
2024-09-01 14:44:30 -0400

    Added in styled action over the person and chat icons.

--- src/Header.js
+++ src/Header.js
@@ -2,20 +2,26 @@ import React from "react";
 import "./Header.css";
 import PersonIcon from "@mui/icons-material/Person";
 import ForumIcon from "@mui/icons-material/Forum";
+import IconButton from "@mui/material/IconButton";
+
 
 const Header = () => {
-return (
-  <>
-    <div className="header">
-      <PersonIcon className="header__icon" fontSize="large" />
-      <img
-        className="header__logo"
-        src="https://seeklogo.com/images/T/tinder-logo-FAAE852EC0-seeklogo.com.png"
-        alt="tinder icon"
-      />
-      <ForumIcon className="header__icon" fontSize="large" />\
-    </div>
-  </>
-);
-}
-export default Header
+  return (
+    <>
+      <div className="header">
+        <IconButton>
+          <PersonIcon className="header__icon" fontSize="large" />
+        </IconButton>
+        <img
+          className="header__logo"
+          src="https://seeklogo.com/images/T/tinder-logo-FAAE852EC0-seeklogo.com.png"
+          alt="tinder icon"
+        />
+        <IconButton>
+          <ForumIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      </div>
+    </>
+  );
+};
+export default Header;
